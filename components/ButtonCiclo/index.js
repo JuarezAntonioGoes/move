cde4a47e0a3771e3ba9f8dfadf2ae1964c795d9f
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-02-27 17:15:57 -0300

    init project

---
+++ components/ButtonCiclo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./style";
+
+const ButtonCiclo = ({ children }) => {
+  return (
+    <Button>
+      {children}
+    </Button>
+  );
+};
+
+export default ButtonCiclo;
---
+++ components/ButtonCiclo/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 24.31rem;
+  height: 5rem;
+  color: #fff;
+  background-color: #5965e0;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 1.25rem;
+  font-family: "Inter", sans-serif;
+`;
+
+export default Button;
